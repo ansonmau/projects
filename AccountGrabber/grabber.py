223798a9db6012fbd15c65df65d64179ccbadf7d
Anson Mau <ansonmau172@gmail.com>
2021-04-14 11:19:20 -0400

    account tool created

--- AccountGrabber/grabber.py
+++ AccountGrabber/grabber.py
@@ -1,19 +1,52 @@
-import os
+import pyperclip
+from vars import path_accounts_file
+
+
+def SpecialPrint(message):
+    if len(message) > 70:
+        border = '-' * 70
+    else:
+        border = '-' * len(message)
+
+    print("{}\n{}\n{}".format(border, message, border))
 
 
 def main():
 
-    accountFile = open(
-        "F:\\Users\\AnsonM\\Documents\\GitHub\\projects\\assets\\accounts.txt", "r")
+    accountFile = open(path_accounts_file, "r")
 
-    accountNumber = 1
+    accountData = []
+    accountNumber = 0
     for line in accountFile:
-        accountData = line.split(":")
+        # remove the \n at the end
+        line = line.strip()
 
-        # first item is the username
-        print("[{}]. {}".format(accountNumber, accountData[0]))
+        accountData.append(line.split(":"))
+
+        # print out the ign and the number option next to it
+        print("[{:2}] {}".format(accountNumber+1,
+                                 accountData[accountNumber][0]))
 
         accountNumber += 1
+
+    choice = input("#: ")
+
+    # make sure the choice is valid
+    while int(choice) not in range(1, len(accountData)+1):
+        print("Enter smthn 1 - {}".format(len(accountData)))
+        choice = input("#: ")
+
+    # get the choice in terms of indices and convert to int
+    choice = int(choice) - 1
+
+    username = accountData[choice][1]
+    password = accountData[choice][2]
+
+    pyperclip.copy(username)
+    input("::> Username copied. Press enter to copy password.")
+
+    pyperclip.copy(password)
+    print("::> Password copied.")
 
     return
 
