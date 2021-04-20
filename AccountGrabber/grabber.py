a0758e5f3f4cc20a399e641cf767faf5c9c813d6
Anson Mau <ansonmau172@gmail.com>
2021-04-20 16:47:01 -0400

    Added some more features for some more fun :)

--- AccountGrabber/grabber.py
+++ AccountGrabber/grabber.py
@@ -1,45 +1,151 @@
 import pyperclip
+from time import sleep
 from os import system
 from vars import path_accounts_file
 
+# Constants
+DIV = "-" * 70
+IND_IGN = 0
+IND_RANK = 1
+IND_BAN = 2
+IND_USER = 3
+IND_PASS = 4
 
-def main():
-    system('cls')
 
-    accountFile = open(path_accounts_file, "r")
+def getRank(string):
+
+    if len(string) < 2:
+        rank = "Unranked"
+    else:
+        rank_list = {
+            'D': 'Diamond',
+            'P': 'Platinum',
+            'G': 'Gold',
+            'S': 'Silver',
+            'B': 'Bronze',
+            'I': 'Iron'
+        }
+        rank_letter = string[0]
+        rank_division = string[1]
+        rank_tier = rank_list[rank_letter]
+        rank = "{} {}".format(rank_tier, rank_division)
+
+    return rank
+
+
+def updateInfo(accountData):
+    try:
+        print("UPDATE...")
+        print("[1] Rank\n[2] Ban")
+        choice = input("> ")
+
+        account_num = int(input("Account #: "))
+
+        # numbers are +1 from actual index
+        account_num -= 1
+
+        # boolean to check if any data was actually changed.
+        change = True
+
+        if choice == "1":
+            new_rank = input("Example: G4\nBlank = unranked\nNew rank: ")
+            accountData[account_num][IND_RANK] = new_rank.upper()
+        elif choice == "2":
+            new_ban = input("Banned until: ")
+            if ',' in new_ban:
+                print("can't have ','")
+            else:
+                accountData[account_num][IND_BAN] = new_ban
+        else:
+            print("Bad input.")
+            change = False
+    except:
+        print("ERROR! Aborting.")
+        change = False
+    if change:
+        accountFile = open(path_accounts_file, "w")
+
+        for account in accountData:
+            line = ",".join(account)
+            accountFile.write(line + "\n")
+
+        print("Updated!")
+
+        accountFile.close()
+
+    return
+
+
+def GetAccountData():
+    accountFile = open(path_accounts_file, "r+")
 
     accountData = []
-    accountNumber = 0
+
     for line in accountFile:
         # remove the \n at the end
         line = line.strip()
 
-        accountData.append(line.split(":"))
+        accountData.append(line.split(","))
+
+        rank = getRank(accountData[-1][IND_RANK])
+
+        ban = accountData[-1][IND_BAN]
+        if ban:
+            ban = "Banned until {}".format(ban)
 
         # print out the ign and the number option next to it
-        print("[{:2}] {}".format(accountNumber+1,
-                                 accountData[accountNumber][0]))
+        print("[{:2}] {:15} | {:^10} | {}".format(len(accountData),
+                                                  accountData[-1][IND_IGN],
+                                                  rank, ban))
 
-        accountNumber += 1
+    accountFile.close()
 
-    choice = input("#: ")
+    return accountData
 
-    # make sure the choice is valid
-    while int(choice) not in range(1, len(accountData)+1):
-        print("Enter smthn 1 - {}".format(len(accountData)))
-        choice = input("#: ")
 
-    # get the choice in terms of indices and convert to int
-    choice = int(choice) - 1
+def main():
+    RUN = True
 
-    username = accountData[choice][1]
-    password = accountData[choice][2]
+    system('cls')
 
-    pyperclip.copy(username)
-    input("::> Username copied. Press enter to copy password.")
+    while RUN:
 
-    pyperclip.copy(password)
-    print("::> Password copied.")
+        accountData = GetAccountData()
+
+        while True:
+            choice = input("#: ")
+
+            if choice in {'x', 'exit', ''}:
+                RUN = False
+                print("Goodbye.")
+                break
+            elif choice == "update":
+                updateInfo(accountData)
+                sleep(1)
+                system('cls')
+                GetAccountData()
+                continue
+            # if the user is not trying to exit, make sure the choice is valid
+            elif int(choice) not in range(1, len(accountData)+1):
+                print("Enter smthn 1 - {}".format(len(accountData)))
+            else:
+                break
+
+        if RUN:
+            # get the choice in terms of indices and convert to int
+            choice = int(choice) - 1
+
+            # the first and last values are always going to be user then pass (in case I add other things)
+            username = accountData[choice][-2]
+            password = accountData[choice][-1]
+
+            pyperclip.copy(username)
+            input("::> Username copied. Press enter to copy password.")
+
+            pyperclip.copy(password)
+            print("::> Password copied.")
+            sleep(1)
+            system('cls')
 
     return
 
